eb559c31bd39745c448d93f4949c2d62983adb1b
J. Gavin Ray <jgavinray@gmail.com>
2015-12-07 12:32:17 -0800

    TokenAuth middleware - initial

--- basicAuthentication.go
+++ basicAuthentication.go
@@ -32,8 +32,36 @@ func BasicAuth(pass http.HandlerFunc) http.HandlerFunc {
 }
 
 func Validate(username, password string) bool {
+    // Make come database call to validate username and password
     if username == "foo" && password == "bar" {
         return true
     }
     return false
+}
+
+func TokenAuth(pass http.HandlerFunc) http.HandlerFunc {
+
+        return func(w http.ResponseWriter, r *http.Request) {
+            auth := strings.SplitN(r.Header["Authorization"][0], " ", 2)
+
+        if len(auth) != 2 || auth[0] != "Bearer" {
+            http.Error(w, "bad syntax", http.StatusBadRequest)
+            return
+        }
+
+        payload, _ := base64.StdEncoding.DecodeString(auth[1])
+
+        if !ValidateToken(payload) {
+            http.Error(w, "authorization failed", http.StatusUnauthorized)
+            return
+        }
+        fmt.Fprintf(w, "Before\n")
+        pass(w, r)
+        fmt.Fprintf(w, "After\n")
+    }
+}
+func ValidateToken(tkn []byte) bool {
+    
+    // Check signature of 
+    return true
 }
