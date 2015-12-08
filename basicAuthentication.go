10dbb336eea434382511c4383d717d5d746708cf
J. Gavin Ray <jgavinray@gmail.com>
2015-12-08 05:11:01 -0800

    basic authentication with crude middleware

--- basicAuthentication.go
+++ basicAuthentication.go
@@ -15,21 +15,26 @@ func BasicAuth(pass http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		auth := strings.SplitN(r.Header["Authorization"][0], " ", 2)
 
+		// Check to see if the Authorization Header has two parts.  A "Basic" followed by something
 		if len(auth) != 2 || auth[0] != "Basic" {
 			http.Error(w, "bad syntax", http.StatusBadRequest)
 			return
 		}
 
+		// Get the second element of the auth array, and attempt to base64 decode it
 		payload, _ := base64.StdEncoding.DecodeString(auth[1])
+		// Split the username:password string into two array elements
 		pair := strings.SplitN(string(payload), ":", 2)
 
+		// Check to make sure we have two elements in the pair array, or that we can confirm the credentials are valid
 		if len(pair) != 2 || !Validate(pair[0], pair[1]) {
 			http.Error(w, "authorization failed", http.StatusUnauthorized)
 			return
 		}
-		fmt.Fprintf(w, "Before\n")
+
+		// If both of the above checks pass, forward the original request to the appropriate function.
 		pass(w, r)
-		fmt.Fprintf(w, "After\n")
+
 	}
 }
 
@@ -46,13 +51,16 @@ func TokenAuth(pass http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		auth := strings.SplitN(r.Header["Authorization"][0], " ", 2)
 
+		// Check to see if the Authorization Header has two parts.  A "Bearer" followed by something
 		if len(auth) != 2 || auth[0] != "Bearer" {
 			http.Error(w, "bad syntax", http.StatusBadRequest)
 			return
 		}
 
+		// Get the second element of the auth array, and attempt to base64 decode it
 		payload, _ := base64.StdEncoding.DecodeString(auth[1])
 
+		// Attempt to Validate what was sent in the second array element of the Authorization header
 		if !ValidateToken(payload) {
 			http.Error(w, "authorization failed", http.StatusUnauthorized)
 			return
@@ -62,28 +70,44 @@ func TokenAuth(pass http.HandlerFunc) http.HandlerFunc {
 		fmt.Fprintf(w, "After\n")
 	}
 }
+
 func ValidateToken(tkn []byte) bool {
 
-	// Check signature of
-	return true
+	tokenToBeValidated := string(tkn)
+	token, err := jwt.Parse(tokenToBeValidated, func(token *jwt.Token) (interface{}, error) {
+
+		// Check What Algorithm Signed the Token
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// Get the key used to sign the token in generateToken
+		return GetSigningKey(), nil
+	})
+
+	if err == nil && token.Valid {
+		return true
+	} else {
+		return false
+	}
 }
 
-func generateToken(w http.ResponseWriter, r *http.Request) {
-
+func GetSigningKey() []byte {
+	// Read File, Database Call, Environment Variable, whereever we want to store the key
 	mySigningKey := []byte(os.Getenv("SuperSecretKey"))
 
 	if mySigningKey == nil {
 		mySigningKey = []byte("SecretLike")
 	}
 
-	r.ParseForm()
+	return mySigningKey
+}
 
-	// logic part of log in
-	username := r.Form["username"]
-	password := r.Form["password"]
-	fmt.Fprintf(w, "%s:%s\n", username, password)
+func generateToken(w http.ResponseWriter, r *http.Request) {
 
-	token := jwt.New(jwt.SigningMethodHS256)
+	mySigningKey := GetSigningKey()
+
+	token := jwt.New(jwt.SigningMethodHS512)
 	// Set some claims
 	token.Claims["foo"] = "bar"
 	token.Claims["nbf"] = time.Now()
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/authentication", BasicAuth(generateToken))
+	http.HandleFunc("/hello", TokenAuth(resource))
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func resource(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!\n")
 }
