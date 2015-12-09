e7e2fe5a16050e578b169cf317146f13881f41b8
J. Gavin Ray <jgavinray@gmail.com>
2015-12-08 19:46:56 -0800

    First Unit Test

---
+++ basicAuthentication_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestValidateUser(t *testing.T) {
+	var tests = []struct {
+		username string
+		password string
+		want     bool
+	}{
+		{"foo", "bar", true},
+		{"user", "bar", false},
+		{"foo", "pass", false},
+		{"user", "pass", false},
+	}
+
+	for _, test := range tests {
+		if got := ValidateUser(test.username, test.password); got != test.want {
+			t.Error("ValidateUser(%q, %q) = %v", test.username, test.password, got)
+		}
+	}
+
+}
